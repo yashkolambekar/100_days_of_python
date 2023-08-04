1ff9e5ae34c3ec908ae3ef95e1de1497201cb7ba
Yash <yashskolambekar2015@gmail.com>
2023-08-04 19:53:34 +0530

    Day 3

---
+++ 003.py
@@ -0,0 +1,55 @@
+# Day 3, lack of motivation, feeling tired, but let's fucking goooo
+
+
+###################################### ROLLERCOASTER
+# print("Welcome to the rollercoaster")
+# height = int(input("What is your height in cm?"))
+# age = int(input("What is your age in years?"))
+# if height > 120:
+#     if age < 12:
+#         print("You have to pay 5$")
+#     elif age >= 12 and age <= 18:
+#         print("You have to pay 8$")
+#     elif age > 18:
+#         print("You have to pay 10$")
+
+# else:
+#     print("You are shorter than the required height")
+
+
+####################################### ODD OR EVEN
+# print("Odd or Even");
+# number = int(input("Enter your number: "))
+# if number %  2 == 0:
+#     print(f"{number} is even")
+# else:
+#     print(f"{number} is odd")
+
+
+####################################### BMI CALCULATOR ADVANCED
+# height  = float(input("enter your height in m: "))
+# weight  = float(input("enter your weight in kg: "))
+# bmi = (weight/(height ** 2))
+
+# if bmi <= 18.5:
+#     print("You are underweight")
+# elif bmi > 18.5 and bmi <= 25:
+#     print("You have normal weight")
+# elif bmi > 25 and bmi <= 30:
+#     print("You are overweight")
+# elif bmi > 30 and bmi <= 35:
+#     print("You are obese")
+# elif bmi > 35:
+#     print("You are clinically obese")
+
+
+####################################### LEAP YEAR CHECKER
+# print("Leap year checker")
+# year = int(input("year: "))
+
+# if (year % 4 == 0) and (year % 100 != 0):
+#     print("This is a leep year")
+# elif (year % 100 == 0) and (year % 400 == 0):
+#     print("This is a leep year (with special case)")
+# else:
+#     print("Not a leap year")
