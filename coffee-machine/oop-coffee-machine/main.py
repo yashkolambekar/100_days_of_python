f36d96785db1466c8c945b9c234d81c96fbaa99d
Yash <yashskolambekar2015@gmail.com>
2023-08-14 18:38:22 +0530

    day 16

--- coffee-machine/oop-coffee-machine/main.py
+++ coffee-machine/oop-coffee-machine/main.py
@@ -2,7 +2,24 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-my_machine = CoffeeMaker()
-my_menu = Menu()
-print(my_menu)
-# input = input(f"What will you like to have good sir? ")
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+coffee_maker.report()
+money_machine.report()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
